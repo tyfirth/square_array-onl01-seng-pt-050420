5826875403febd2893f03033af1274dbb5bfd339
Tyler Firth <tyfirth@gmail.com>
2020-05-11 18:46:44 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,12 +2,10 @@ def square_array(array)
   
   squared = []
   
-  array.each do |number| 
-   
-  squared << number**2
+  array.each { |number|  squared << number**2}
   
   return squared
   
-  end
+  
     
 end
